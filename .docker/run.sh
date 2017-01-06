b4df3bfe0b1f3eb95eb945528d309ceb96083518
James Maddox <jhmaddox@gmail.com>
2017-01-06 14:22:56 -0800

    (feature) add nginx proxy w/static file serving to example config

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sleep 8 # give database time to boot
+sleep 14 # give database time to boot
+python manage.py collectstatic --no-input
 python manage.py migrate
 gunicorn -b 0.0.0.0:8000 private_url_shortener.wsgi:application
